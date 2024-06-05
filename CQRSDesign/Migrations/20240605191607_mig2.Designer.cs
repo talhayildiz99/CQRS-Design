9e325954cdf8b266335b7d56532862842d6fcffe
Talha <99talhayildiz@gmail.com>
2024-06-05 22:17:10 +0300

    add migration

--- CQRSDesign/Migrations/20240605191607_mig2.Designer.cs
+++ CQRSDesign/Migrations/20240605191607_mig2.Designer.cs
@@ -3,6 +3,7 @@ using CQRSDesign.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CQRSDesign.Migrations
 {
     [DbContext(typeof(CQRSContext))]
-    partial class CQRSContextModelSnapshot : ModelSnapshot
+    [Migration("20240605191607_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
